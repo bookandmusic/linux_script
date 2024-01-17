6d2bcb63b3df7bbbbf3a60473d5788f442b06c1e
bookandmusic <bookandmusic@yeah.net>
2024-01-17 18:25:52 +0800

    修改执行命令方式

--- env_init.sh
+++ env_init.sh
@@ -1,15 +1,11 @@
 #! /usr/bin/env bash
 
-OS=Linux
-ARCH=x86_64
-COMMAND_PREX="sudo "
+GITHUB_PROXY=https://mirror.ghproxy.com/
 
-GITHUB_PROXY=https://ghproxy.com/
-
-export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.ustc.edu.cn/brew.git"
-export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.ustc.edu.cn/homebrew-core.git"
-export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.ustc.edu.cn/homebrew-bottles"
-export HOMEBREW_API_DOMAIN="https://mirrors.ustc.edu.cn/homebrew-bottles/api"
+HOMEBREW_BREW_GIT_REMOTE="https://mirrors.ustc.edu.cn/brew.git"
+HOMEBREW_CORE_GIT_REMOTE="https://mirrors.ustc.edu.cn/homebrew-core.git"
+HOMEBREW_BOTTLE_DOMAIN="https://mirrors.ustc.edu.cn/homebrew-bottles"
+HOMEBREW_API_DOMAIN="https://mirrors.ustc.edu.cn/homebrew-bottles/api"
 
 NVM_INSTALL_VERSION="0.39.5"
 NODE_VERSION="16.20.2"
@@ -39,11 +35,23 @@ function error() {
 	exit 1
 }
 
+function run_cmd() {
+	cmd=$1
+	info "CMD: $cmd"
+
+	eval $cmd
+	if [ $? -ne 0 ]; then
+		exit 1
+	fi
+}
+
 function pre_check() {
 	local os=$(uname)
 	if [[ ${os} == "Darwin" ]]; then
 		OS=Mac
+		SED_PREFIX="sed -i '' "
 	elif [[ ${os} == "Linux" ]]; then
+		SED_PREFIX="sed -i "
 		# 检查是否为CentOS
 		if [ -f /etc/redhat-release ]; then
 			OS=CentOS
@@ -74,7 +82,7 @@ function pre_check() {
 	if [ "${user}" == "root" ]; then
 		COMMAND_PREX=""
 	else
-		COMMAND_PREX="sudo"
+		COMMAND_PREX="sudo "
 	fi
 	info "The current USER: ${user}"
 }
@@ -83,21 +91,15 @@ function pre_start() {
 	info "The start install required packages ..."
 	# 1. 判断系统是Linux还是Mac
 	if [[ ${OS} == "Mac" ]]; then
-		echo "xcode-select --install"
+		info "xcode-select --install"
 		xcode-select --install &>/dev/null
 	elif [[ ${OS} == "CentOS" ]]; then
-		echo "${COMMAND_PREX} yum makecache && ${COMMAND_PREX} yum install -y git curl wget zsh vim"
-		{
-			${COMMAND_PREX} yum makecache && ${COMMAND_PREX} yum install -y git curl wget zsh vim
-		} 
-	elif [[ ${OS} == "Debian" ]]; then
-		echo "${COMMAND_PREX} sed -i 's@//.*archive.ubuntu.com@//mirrors.ustc.edu.cn@g' /etc/apt/sources.list && ${COMMAND_PREX} apt update && ${COMMAND_PREX} apt install -y sshpass ca-certificates git curl wget zsh vim"
-		{
-			${COMMAND_PREX} sed -i 's@//.*archive.ubuntu.com@//mirrors.ustc.edu.cn@g' /etc/apt/sources.list &&
-				${COMMAND_PREX} apt update &&
-				${COMMAND_PREX} apt install -y sshpass ca-certificates git curl wget zsh vim
-		} 
+		run_cmd "${COMMAND_PREX}yum makecache && ${COMMAND_PREX}yum install -y git curl wget zsh vim"
 
+	elif [[ ${OS} == "Debian" ]]; then
+		run_cmd "${COMMAND_PREX}sed -i 's@//.*archive.ubuntu.com@//mirrors.ustc.edu.cn@g' /etc/apt/sources.list &&
+				${COMMAND_PREX}apt update &&
+				${COMMAND_PREX}apt install -y sshpass ca-certificates git curl wget zsh vim"
 	fi
 	info "The already install required packages"
 }
@@ -106,12 +108,9 @@ function final_clear() {
 	if [[ ${OS} == "Debian" ]]; then
 		info "The clear ..."
 
-		echo "${COMMAND_PREX} apt-get clean && ${COMMAND_PREX} rm -rf /var/lib/apt/lists/* && ${COMMAND_PREX} rm -rf /src/*.deb"
-		{
-			${COMMAND_PREX} apt-get clean &&
-				${COMMAND_PREX} rm -rf /var/lib/apt/lists/* &&
-				${COMMAND_PREX} rm -rf /src/*.deb
-		} 
+		run_cmd "${COMMAND_PREX}apt-get clean &&
+				${COMMAND_PREX}rm -rf /var/lib/apt/lists/* &&
+				${COMMAND_PREX}rm -rf /src/*.deb"
 
 		info "The clear complete"
 	fi
@@ -120,19 +119,16 @@ function final_clear() {
 function config_localtime() {
 	if [ ! -f "/etc/localtime" ]; then
 		info "create /etc/localtime"
-		echo "${COMMAND_PREX} ln -svf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime"
-		${COMMAND_PREX} ln -svf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime 
+		run_cmd "${COMMAND_PREX}ln -svf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime"
 	fi
 }
 
 function install_vimrc() {
 	info "The start install vimrc ..."
 
-	echo "git clone --depth=1 ${GITHUB_PROXY}https://github.com/amix/vimrc.git ~/.vim_runtime"
-	git clone --depth=1 ${GITHUB_PROXY}https://github.com/amix/vimrc.git ~/.vim_runtime
+	run_cmd "git clone --depth=1 ${GITHUB_PROXY}https://github.com/amix/vimrc.git ~/.vim_runtime"
 
-	echo "sh ~/.vim_runtime/install_awesome_vimrc.sh"
-	sh ~/.vim_runtime/install_awesome_vimrc.sh
+	run_cmd "sh ~/.vim_runtime/install_awesome_vimrc.sh"
 
 	info "The already install vimrc"
 }
@@ -141,26 +137,16 @@ function install_ohmyzsh() {
 	info "The start install ohmyzsh ..."
 
 	waring "execute ohmyzsh install.sh"
-	echo "echo \"Y\" | /bin/bash -c \"\$(curl -fsSL ${GITHUB_PROXY}https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)\""
-	{
-		export REMOTE=${GITHUB_PROXY}https://github.com/ohmyzsh/ohmyzsh.git &&
-			echo "Y" | /bin/bash -c "$(curl -fsSL ${GITHUB_PROXY}https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-	} 
 
-	echo "git clone ${GITHUB_PROXY}https://github.com/zsh-users/zsh-autosuggestions.git \${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
-	{
-		git clone ${GITHUB_PROXY}https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-	} 
+	run_cmd	"curl -fsSL ${GITHUB_PROXY}https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh > install.sh && export REMOTE=${GITHUB_PROXY}https://github.com/ohmyzsh/ohmyzsh.git && echo 'Y' | /bin/bash install.sh && rm install.sh"
 
-	echo "git clone ${GITHUB_PROXY}https://github.com/zsh-users/zsh-syntax-highlighting.git \${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
-	{
-		git clone ${GITHUB_PROXY}https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-	} 
+	run_cmd "git clone ${GITHUB_PROXY}https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
 
-	echo "sed -i 's@plugins=(git)@plugins=(sudo git zsh-autosuggestions zsh-syntax-highlighting)@g' ~/.zshrc"
-	{
-		sed -i 's@plugins=(git)@plugins=(sudo git zsh-autosuggestions zsh-syntax-highlighting)@g' ~/.zshrc
-	} 
+
+	run_cmd "git clone ${GITHUB_PROXY}https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+
+
+	run_cmd "${SED_PREFIX}'s@plugins=(git)@plugins=(sudo git zsh-autosuggestions zsh-syntax-highlighting)@g' ~/.zshrc"
 
 	info "The already install ohmyzsh"
 }
@@ -175,10 +161,7 @@ function install_homebrew() {
 	export HOMEBREW_BREW_GIT_REMOTE=${HOMEBREW_BREW_GIT_REMOTE}
 	export HOMEBREW_CORE_GIT_REMOTE=${HOMEBREW_CORE_GIT_REMOTE}
 
-	echo "/bin/bash -c \"$(curl -fsSL https://mirrors.ustc.edu.cn/misc/brew-install.sh)\""
-	{
-		/bin/bash -c "$(curl -fsSL https://mirrors.ustc.edu.cn/misc/brew-install.sh)"
-	} 
+	run_cmd ' echo -e "\n" | /bin/bash -c "$(curl -fsSL https://mirrors.ustc.edu.cn/misc/brew-install.sh)"'
 
 	waring "config homebrew env"
 
@@ -208,26 +191,18 @@ function install_starship() {
 
 	waring "execute starship install.sh"
 
-	echo "mkdir -p \$HOME/.local/bin && mkdir -p \$HOME/.config"
-	mkdir -p $HOME/.local/bin && mkdir -p $HOME/.config
+	run_cmd "mkdir -p $HOME/.local/bin && mkdir -p $HOME/.config"
 
 	export BASE_URL="${GITHUB_PROXY}https://github.com/starship/starship/releases"
 	export BIN_DIR="$HOME/.local/bin"
 
-	echo "curl -sSL ${GITHUB_PROXY}https://raw.githubusercontent.com/starship/starship/master/install/install.sh > starship_install.sh &&
-			sed -i 's@confirm "Install Starship@# confirm "Install Starship@g' starship_install.sh &&
+	run_cmd "curl -sSL ${GITHUB_PROXY}https://raw.githubusercontent.com/starship/starship/master/install/install.sh > starship_install.sh &&
+			${SED_PREFIX}'s@confirm \"Install Starship@# confirm \"Install Starship@g' starship_install.sh &&
 			sh starship_install.sh &&
 			rm -rf starship_install.sh"
-	{
-		curl -sSL ${GITHUB_PROXY}https://raw.githubusercontent.com/starship/starship/master/install/install.sh >starship_install.sh &&
-			sed -i 's@confirm "Install Starship@# confirm "Install Starship@g' starship_install.sh &&
-			sh starship_install.sh &&
-			rm -rf starship_install.sh
-	} 
 
 	waring "generate starship.toml"
-	echo "export PATH=$PATH:~/.local/bin && starship preset tokyo-night -o ~/.config/starship.toml"
-	export PATH=$PATH:~/.local/bin && starship preset tokyo-night -o ~/.config/starship.toml
+	run_cmd "export PATH=$PATH:~/.local/bin && starship preset tokyo-night -o ~/.config/starship.toml"
 
 	waring "config starship env"
 	echo "" | tee -a ~/.bashrc ~/.zshrc
@@ -242,29 +217,16 @@ function install_gvm() {
 	info "The start install gvm ..."
 
 	if [[ ${OS} == "Mac" ]]; then
-		echo "brew update && brew install mercurial"
-		{
-			brew update && brew install mercurial
-		} 
+		run_cmd "brew update && brew install mercurial"
 	elif [[ ${OS} == "CentOS" ]]; then
-		echo "${COMMAND_PREX} yum install -y gcc bison make glibc-devel"
-		{
-			${COMMAND_PREX} yum install -y gcc bison make glibc-devel
-		} 
+		run_cmd "${COMMAND_PREX}yum install -y gcc bison make glibc-devel"
 	elif [[ ${OS} == "Debian" ]]; then
-		echo "${COMMAND_PREX} apt update && ${COMMAND_PREX} apt install -y mercurial make binutils bison gcc build-essential bsdmainutils"
-		{
-			${COMMAND_PREX} apt update && ${COMMAND_PREX} apt install -y mercurial make binutils bison gcc build-essential bsdmainutils
-		} 
+		run_cmd "${COMMAND_PREX}apt update && ${COMMAND_PREX}apt install -y mercurial make binutils bison gcc build-essential bsdmainutils"
 	fi
 
 	waring "execute gvm-installer"
-	echo "export SRC_REPO=${GITHUB_PROXY}https://github.com/moovweb/gvm.git &&
+	run_cmd "export SRC_REPO=${GITHUB_PROXY}https://github.com/moovweb/gvm.git &&
 			bash < <(curl -sSL ${GITHUB_PROXY}https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)"
-	{
-		export SRC_REPO=${GITHUB_PROXY}https://github.com/moovweb/gvm.git &&
-			bash < <(curl -sSL ${GITHUB_PROXY}https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
-	} 
 
 	waring "config gvm env"
 	echo "" | tee -a ~/.bashrc ~/.zshrc
@@ -282,47 +244,27 @@ function install_gvm() {
 	info "The already install gvm"
 
 	waring "clone go src repo to $HOME/.gvm/archive/go"
-	echo "git ${GITHUB_PROXY}https://github.com/golang/go.git \$HOME/.gvm/archive/go"
-	git clone ${GITHUB_PROXY}https://github.com/golang/go.git $HOME/.gvm/archive/go
+	run_cmd "git clone ${GITHUB_PROXY}https://github.com/golang/go.git $HOME/.gvm/archive/go"
 
 	waring "install go${GO_VERSION}"
-	{
-		gvm install go${GO_VERSION} -B && gvm use go${GO_VERSION} --default
-	} 
+	run_cmd "gvm install go${GO_VERSION} -B && gvm use go${GO_VERSION} --default"
+
 }
 
 function install_pyenv() {
 	info "The start install pyenv ..."
 
 	if [[ ${OS} == "Mac" ]]; then
-		echo "brew install openssl readline sqlite3 xz zlib tcl-tk"
-		{
-			brew install openssl readline sqlite3 xz zlib tcl-tk
-		} 
+		run_cmd "brew install openssl readline sqlite3 xz zlib tcl-tk"
 	elif [[ ${OS} == "CentOS" ]]; then
-		echo "${COMMAND_PREX} yum install -y gcc make patch zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel xz-devel"
-		{
-			${COMMAND_PREX} yum install -y gcc make patch zlib-devel bzip2 bzip2-devel \
-				readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel xz-devel
-		} 
+		run_cmd "${COMMAND_PREX}yum install -y gcc make patch zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel xz-devel"
 	elif [[ ${OS} == "Debian" ]]; then
-		echo "${COMMAND_PREX} apt update && ${COMMAND_PREX} apt install -y build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev"
-		{
-			${COMMAND_PREX} apt update && ${COMMAND_PREX} apt install -y build-essential libssl-dev zlib1g-dev \
-				libbz2-dev libreadline-dev libsqlite3-dev curl \
-				libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-		} 
+		run_cmd "${COMMAND_PREX}apt update && ${COMMAND_PREX}apt install -y build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev"
 	fi
 
-	echo "git clone ${GITHUB_PROXY}https://github.com/yyuu/pyenv.git ~/.pyenv"
-	{
-		git clone ${GITHUB_PROXY}https://github.com/yyuu/pyenv.git ~/.pyenv
-	} 
+	run_cmd "git clone ${GITHUB_PROXY}https://github.com/yyuu/pyenv.git ~/.pyenv"
 
-	echo "mkdir -p ~/.pyenv/plugins && git clone ${GITHUB_PROXY}https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv"
-	{
-		mkdir -p ~/.pyenv/plugins && git clone ${GITHUB_PROXY}https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
-	} 
+	run_cmd "mkdir -p ~/.pyenv/plugins && git clone ${GITHUB_PROXY}https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv"
 
 	waring "config pyenv env"
 	echo "" | tee -a ~/.bashrc ~/.zshrc
@@ -344,21 +286,16 @@ function install_pyenv() {
 	info "The already install pyenv"
 
 	info "install python ${PYTHON_VERSION}"
-	{
-		pyenv install ${PYTHON_VERSION} &&
+	run_cmd "pyenv install ${PYTHON_VERSION} &&
 			pyenv global ${PYTHON_VERSION} &&
 			pip install -i ${PYPI_PROXY} pip -U &&
-			pip config set global.index-url ${PYPI_PROXY}
-	} 
+			pip config set global.index-url ${PYPI_PROXY}"
 }
 
 function install_nvm() {
 	info "The start install nvm ..."
 
-	echo "git clone ${GITHUB_PROXY}https://github.com/nvm-sh/nvm.git ~/.nvm && git -C ~/.nvm checkout v${NVM_INSTALL_VERSION}"
-	{
-		git clone ${GITHUB_PROXY}https://github.com/nvm-sh/nvm.git ~/.nvm && git -C ~/.nvm checkout v${NVM_INSTALL_VERSION}
-	} 
+	run_cmd "git clone ${GITHUB_PROXY}https://github.com/nvm-sh/nvm.git ~/.nvm && git -C ~/.nvm checkout v${NVM_INSTALL_VERSION}"
 
 	waring "config nvm env"
 	echo "" | tee -a ~/.bashrc ~/.zshrc
@@ -377,16 +314,12 @@ function install_nvm() {
 	info "The already install nvm"
 
 	info "install node ${NODE_VERSION}"
-	{
-		nvm install ${NODE_VERSION} && nvm alias default ${NODE_VERSION}
-	} 
+	run_cmd "nvm install ${NODE_VERSION} && nvm alias default ${NODE_VERSION}"
 
-	{
-		export PATH=$PATH:$HOME/.nvm/versions/node/v${NODE_VERSION}/bin &&
+	run_cmd "export PATH=$PATH:$HOME/.nvm/versions/node/v${NODE_VERSION}/bin &&
 			npm install --registry ${NPM_PROXY} -g yarn &&
 			npm config set registry ${NPM_PROXY} &&
-			yarn config set registry ${NPM_PROXY}
-	} 
+			yarn config set registry ${NPM_PROXY}"
 }
 
 function main() {
@@ -396,12 +329,11 @@ function main() {
 	config_localtime
 	install_vimrc
 	install_ohmyzsh
-	install_homebrew
 	install_starship
+	install_homebrew
 	install_gvm
-	install_nvm
 	install_pyenv
-	final_clear
+	install_nvm
 }
 
 main
